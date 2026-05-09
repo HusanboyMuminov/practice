76144685ce949f2331d2a0e163720ac1756a6912
Husanboymuminov <xusanboymuminov2161@gmail.com>
2026-05-09 21:38:59 +0900

    feat: learn class and  its ordinary and static property

---
+++ class.about.py
@@ -0,0 +1,38 @@
+'''CLASS
+(1) What is class
+(2) ordinary vs static properties
+(3) special methods
+'''
+
+print("===== What is class =====")
+# class - blueprint for object creation!
+# structure > state constructor method
+
+
+class Person():
+    # state 
+    message = "class state property"
+    
+    # constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    # method
+    def introduce(self):
+        print(f"{self.name} says: How do you do!")
+        
+    def say_age(self):
+        print(f"{self.name} says I am {self.age}!")
+        
+        
+person1 = Person("Justin", 25)
+person2 = Person("Martin", 35)
+person3 = Person("John", 22)
+
+# ordinary state
+print("person1.name:", person1.name)
+
+# ordinary method
+person1.introduce()
+person2.say_age()                 
